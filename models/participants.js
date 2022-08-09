6a047f75e0f94334a38d1ccc107760c89cf27b36
Francisco Rones <ininorones@gmail.com>
2022-08-09 00:10:56 -0700

    Update: Committed some changes

---
+++ models/participants.js
@@ -0,0 +1,15 @@
+module.exports = (sequelize, DataTypes) => {
+  const Participants = sequelize.define("Users", {
+    user_id: {
+      type: DataTypes.INTEGER,
+      autoIncrement: true,
+      allowNull: false,
+      primaryKey: true,
+    },
+    username: { type: DataTypes.STRING, allowNull: false, unique: true },
+    email: { type: DataTypes.STRING, allowNull: false },
+    password: { type: DataTypes.STRING, allowNull: false },
+    birthday: { type: DataTypes.DATEONLY, allowNull: false },
+  });
+  return Participants;
+};
